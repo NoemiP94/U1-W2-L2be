004cc7b768758ad0b472d0a42e89c95b26b68630
NoemiP94 <m_emi94@hotmail.it>
2023-12-05 15:16:22 +0100

    add first exercise

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,5 +1,44 @@
+package es1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci il numero di elementi: ");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Set<String> paroleDistinte = new HashSet<>();
+        Set<String> paroleDuplicate = new HashSet<>();
+
+        System.out.println("Inserisci " + n + " parole: ");
+        for(int i = 0; i < n; i++){
+            String parola = scanner.nextLine();
+
+            if(!paroleDistinte.add(parola)){
+                paroleDuplicate.add(parola);
+            }
+        }
+
+        System.out.println("Parole duplicate: ");
+        for(String parola : paroleDuplicate){
+            System.out.println(parola);
+        }
+
+        System.out.println("Numero parole distinte; " + paroleDistinte.size());
+        System.out.println("Elenco delle parole distinte: ");
+        for(String parola : paroleDistinte){
+            System.out.println(parola);
+        }
+
+        scanner.close();
+
+
     }
 }
